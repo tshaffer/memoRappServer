12751372a5c41cc87fcd7146303c18aaec4c18f2
Ted Shaffer <ted@brightsign.biz>
2019-09-17 12:48:18 -0700

    heroku specific changes

--- server.js
+++ server.js
@@ -22,4 +22,10 @@ app.get('*', function (req, res) {
   }
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
