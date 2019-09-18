afa3e91f90565ad021f8fe6059db3c91c87505c4
Ted Shaffer <ted@brightsign.biz>
2019-09-17 17:47:30 -0700

    debugging info

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
 }
+console.log('listen on port: ' + port)
 app.listen(port);
 
 // app.listen(port, () => console.log(`Example app listening on port ${port}!`))
